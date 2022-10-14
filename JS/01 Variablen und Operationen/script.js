21a060524dafd90f7f7444663e1ccae8b51286a0
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-10-14 09:00:10 +0200

    Finished Task 01

---
+++ JS/01 Variablen und Operationen/script.js
@@ -0,0 +1,33 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+var result = a + b + c
+console.log(result)
+
+// (a + b) / c
+var result = (a + b) / c
+console.log(result)
+
+// c -a + b
+var result = c -a + b
+console.log(result)
+
+// a + b + c * 3
+var result = a + b + c * 3
+console.log(result)
+
+// (a + b + c)/4
+var result = (a + b + c)/4
+console.log(result)
+
+// Ändere die Variablen auf folgende Werte
+ a = 3
+ b = a + 3
+ c = c -3
+// gib die neuen Werte der Variablen aus
+console.log(a, b, c)
