710037eadca16d93aa58248e6ba9d3636cc6a6fa
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-10-18 12:03:34 +0200

    Finished Assignment 04 Loops

---
+++ JS/04 Schleifen/script.js
@@ -0,0 +1,42 @@
+// Zähle von 7 bis 45
+for (let i = 6; i < 45; i++) {
+    console.log(i + 1);
+    
+}
+
+// Zähle von 9 bis 99
+for (let i = 8; i < 99; i++) {
+    console.log(i + 1);
+    
+}
+
+// Zähle rückwärts von 10 bis 0
+for (let i = 11; i > 0; i--) {
+    console.log(i - 1);
+    
+}
+
+// Zähle rückwärts von 1234 bis 1207
+for (let i = 1235; i > 1207; i--) {
+    console.log(i - 1);
+    
+}
+
+// Gib Deinen Namen genau 8 mal aus
+for (let i = 0; i < 8; i++) {
+    console.log("Alex");
+    
+}
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+    
+}
+
+console.log("Alex");
+
+for (let i = 11; i > 0; i--) {
+    console.log(i - 1);
+    
+}
