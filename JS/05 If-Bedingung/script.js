d8aba2fa390421e5c0c1240574509df9c69192ac
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-10-18 12:15:05 +0200

    Finished Assignment 05 If-Clauses

---
+++ JS/05 If-Bedingung/script.js
@@ -0,0 +1,31 @@
+// Aufgabe 1
+let numberOne = 3;
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if(numberOne < 20) {
+    console.log("Mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+if(numberOne > 20 && numberOne < 50) {
+    console.log("Medium");
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if (numberOne > 50) {
+    console.log("Large");
+}
+
+// Aufgabe 2
+let numberTwo  = 5;
+
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (numberTwo < 7) {
+    console.log("Mini");
+}
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+if (numberTwo > 7 && numberTwo < 90) {
+    console.log("Medium");
+}
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+if (numberTwo > 90) {
+    console.log("Large");
+}
