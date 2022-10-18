9f1323d2b169e3a936b59f213c65f66d3f8bb820
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-10-18 12:34:02 +0200

    Finished Assignment 07 Logical Operators

---
+++ JS/07 Logische Operatoren/script.js
@@ -0,0 +1,21 @@
+//Aufgabe 1
+// Erstelle zwei Zufallszahl zwischen 0 und 100
+let randomHundred = Math.random() * 100;
+
+// Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50, dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+let randomHundred1 = Math.random() * 100;
+let randomHundred2 = Math.random() * 100;
+
+if (randomHundred1 < randomHundred2 && randomHundred1 < 50) {
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");    
+}
+
+// Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30 dann gib aus "Eine der beiden ist kleiner als 30"
+if (randomHundred1 < 30 || randomHundred2 < 30) {
+    console.log("Eine der beiden ist kleiner als 30");
+}
+
+// Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist dann gib aus "Erste Zahl klein, zweite kein 50iger"
+if (randomHundred1 < 50 && randomHundred2 != 50) {
+    console.log("Erste Zahl klein, zweite kein 50iger");
+}
