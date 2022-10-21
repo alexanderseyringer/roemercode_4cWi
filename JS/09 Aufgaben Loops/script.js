1ee2330a5b416d5e444e16701bdcee38a003b9b4
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-10-21 08:58:40 +0200

    Finished Assignment 09

---
+++ JS/09 Aufgaben Loops/script.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 4; i++) {
+    let rowNumber = " ";
+    for (let i2 = 0; i2 < 4; i2++) {
+        if (i%2!=0) {
+            rowNumber += "O";
+        }
+
+        else {
+            rowNumber += "X";
+        } 
+    }
+    console.log(rowNumber);
+}
