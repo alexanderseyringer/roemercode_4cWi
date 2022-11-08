9981976892e8015e2ff302cf4f8edf1057e39308
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-11-08 11:41:39 +0100

    Finished 2nd Part of Assignment 09

--- JS/09 Aufgaben Loops/script.js
+++ JS/09 Aufgaben Loops/script.js
@@ -1,3 +1,5 @@
+// Aufgabe 1
+
 for (let i = 0; i < 4; i++) {
     let rowNumber = "";
     for (let i2 = 0; i2 < 4; i2++) {
@@ -10,4 +12,14 @@ for (let i = 0; i < 4; i++) {
         } 
     }
     console.log(rowNumber);
+}
+
+// Aufgabe 2
+// Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)
+let number = 0;
+for (let i = 1; i < 101; i++) {
+        if (i%2 == 0) {
+            number += i;
+            console.log(number);
+        }
 }
