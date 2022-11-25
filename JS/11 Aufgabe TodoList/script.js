3f9166756ec72e7b83af14b72b6942ad646abc84
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-11-25 09:00:20 +0100

    Added Checkboxes with checkbox memory and finished

--- JS/11 Aufgabe TodoList/script.js
+++ JS/11 Aufgabe TodoList/script.js
@@ -1,19 +1,46 @@
-let taskSaver = ["Gscheid Putzen", "Besser Putzen", "Genial Putzen"];
+let taskJSON1 = {name: "Gscheid Putzen", isDone: true, responsible: "Fast niemand"}
+let taskJSON2 = {name: "Besser Putzen", isDone: false, responsible: "Noch weniger"}
+let taskJSON3 = {name: "Genial Putzen", isDone: false, responsible: "Niemand"}
+
+let tasksArray = [taskJSON1, taskJSON2, taskJSON3];
+
+writeTasks();
 
 document.getElementById("taskAdder").addEventListener("click", function(){
-    let additionalTask = document.getElementById("newTaskTxt").value;
-    taskSaver.push(additionalTask)
-    document.getElementById("taskList").innerHTML = getTasksToHTML();
+    addTasks();
+    writeTasks();
 });
 
-document.getElementById("taskList").innerHTML = getTasksToHTML();
+function writeTasks() {
+    document.getElementById("taskList").innerHTML = getTasksToHTML();
+}
 
+function addTasks() {
+    let additionalTask = document.getElementById("newTaskTxt").value;
+    let additionalResponsible = document.getElementById("newResponsibleTxt").value;
+    let task = {name: additionalTask, responsible: additionalResponsible, isDone: false}
+    tasksArray.push(task)
+}
+
+function checkTask(element) {
+    let index = element.attributes["data-index"].value;
+    let isChecked = element.checkerChecked;
+
+    tasksArray[index].isDone = isChecked;
+    writeTasks();
+}
 
 function getTasksToHTML() {
     let html = "";
+    let index = 0;
 
-    taskSaver.forEach(element => {
-        html += "<li>" + element + "";
+    tasksArray.forEach(element => {
+        let checkerChecked = "";
+        if(element.isDone) {
+            checkerChecked = "checked";
+        }
+        html += "<li><input onClick='checkTask' name='checkbox' data-index='" + index + "'input type='checkbox'" + checkerChecked + "/>" + " " + element.name + " - " + element.responsible + "";
+        index++;
     });
 
     return html;
