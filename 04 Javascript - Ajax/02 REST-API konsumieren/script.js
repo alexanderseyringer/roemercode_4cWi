77c8a0df6a87eb42e544bbf26222e44a51eb0538
Alexander Seyringer <alexander.seyringer@outlook.at>
2022-12-16 08:09:32 +0100

    Commiting a so called "Zwischenstand"

--- 04 Javascript - Ajax/02 REST-API konsumieren/script.js
+++ 04 Javascript - Ajax/02 REST-API konsumieren/script.js
@@ -1,3 +1,17 @@
-document.getElementById("loader").addEventListener("click") = fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(response => response.json)
-    .then(json)
+document.getElementById("loader").addEventListener("click", function() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((result)=> {
+        result.json().then((data)=>{
+            getHTMLForToDos(data);
+        })
+    })
+})
+
+    function getHTMLForToDos(data) {
+        let html = "<ul>";
+        data.forEach(todo => {
+            html += "<li>" + todo.title + "</li>";
+        });
+        html += "</ul>";
+        document.getElementById("content").innerHTML = html;
+    }
