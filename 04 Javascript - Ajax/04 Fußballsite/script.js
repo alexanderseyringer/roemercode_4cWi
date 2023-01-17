a4627bccbd6549315a37dd78372095e88f4f76c4
Alexander Seyringer <alexander.seyringer@outlook.at>
2023-01-17 12:36:17 +0100

    Finished Fußballsite with minor Bugs

--- 04 Javascript - Ajax/04 Fußballsite/script.js
+++ 04 Javascript - Ajax/04 Fußballsite/script.js
@@ -1,10 +1,35 @@
+let finalHTML;
+let teamName1;
+let teamName2;
+let points1;
+let points2;
+
 function importer() {
     fetch("https://api.openligadb.de/getmatchdata/WM2022/2022").then((result)=> {
         result.json().then((data)=>{
-            getHTMLForToDos(data);
-            console.log(data.leagueName);
+            getData(data);
         })
     })
 }
 
-importer();
+function getData(data) {
+    let length = data.length;
+    for (let i = 0; i < 4; i++) {
+        teamName1 = data[i].team1.teamName;
+        teamName2 = data[i].team2.teamName;
+        points1 = data[i].matchResults[0].pointsTeam1;
+        points2 = data[i].matchResults[0].pointsTeam2;
+        console.log(teamName1, teamName2, points1, points2);
+        finalHTML += getHTML(teamName1, teamName2, points1, points2);
+    }
+    document.getElementById("content").innerHTML = finalHTML;
+}
+
+function getHTML(teamName1, teamName2, points1, points2) {
+    let html = "<ul>";
+    html += "<li>" + teamName1 + " : " + points1 + " " + points2 + "</li>";
+    html += "</ul>";
+    return html;
+};
+
+importer() // JA NED WEGLÖSCHEN!!!
